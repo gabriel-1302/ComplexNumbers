746a4bde64ec96f65d309b15f969f405e39dfdd6
Valeria Alexandra Cuellar Coca <valeriaalexandracuellarcoca@gmail.com>
2024-03-08 03:26:12 -0400

    Update Complex.java
    Creación de los métodos sumar, restar y multiplicar

--- Complex.java
+++ Complex.java
@@ -1,8 +1,6 @@
 import java.lang.Math;
 import java.util.Scanner;
 
-
-
 public class Complejo {
     
     private double real;
@@ -14,11 +12,25 @@ public class Complejo {
         this.imaginario = imaginario;
     }
 
+    //@valeriaalexandracuellarcoca
+    // Método para sumar dos números complejos
+    public Complejo sumar(Complejo otroComplejo) {
+        return new Complejo(this.real + otroComplejo.real, this.imaginario + otroComplejo.imaginario);
+    }
 
+    // Método para restar dos números complejos
+    public Complejo restar(Complejo otroComplejo) {
+        return new Complejo(this.real - otroComplejo.real, this.imaginario - otroComplejo.imaginario);
+    }
+
+    // Método para multiplicar dos números complejos
+    public Complejo multiplicar(Complejo otroComplejo) {
+        double nuevoReal = (this.real * otroComplejo.real) - (this.imaginario * otroComplejo.imaginario);
+        double nuevoImaginario = (this.real * otroComplejo.imaginario) + (this.imaginario * otroComplejo.real);
+        return new Complejo(nuevoReal, nuevoImaginario);
+    }
     public static void main(String[] args) {
 
-
-
-        
+     
     }
 }
